3d1216beef40e5e59faf4f83539c97d5c21c297b
ElPanaDero1224 <samelibb10@gmail.com>
2025-02-26 11:32:00 -0600

    X

--- database.py
+++ database.py
@@ -2,8 +2,6 @@ from sqlalchemy import create_engine, MetaData
 from databases import Database
 from decouple import config
 
-# Configura la URL de la base de datos (modifica según tu necesidad)
-#DATABASE_URL = "mysql+aiomysql://root:root@localhost:3306/sistemaintegral"
 DATABASE_URL = config("DATABASE_URL")
 
 # Crea el motor de SQLAlchemy y la conexión asíncrona
